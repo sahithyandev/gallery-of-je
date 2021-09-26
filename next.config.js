38b0de30c17638d06d92d529804412258b1c9e26
Sahithyan <sahithyan2701@gmail.com>
2021-09-26 11:24:41 +0530

    preact on production

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 module.exports = {
 	images: {
-		domains: ["erwfxmftkzktexefxtdy.supabase.in", "gallery-of-je.imgix.net"]
+		domains: ["erwfxmftkzktexefxtdy.supabase.in", "gallery-of-je.imgix.net"],
+	},
+	webpack: (config, { dev, isServer }) => {
+		// Replace React with Preact only in client production build
+		if (!dev && !isServer) {
+			Object.assign(config.resolve.alias, {
+				react: "preact/compat",
+				"react-dom/test-utils": "preact/test-utils",
+				"react-dom": "preact/compat",
+			});
+		}
+
+		return config;
 	},
 };
