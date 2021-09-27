b5c4df19265c358907f2a9c8ec95dc46abb9a8b2
Sahithyan <Sahithyan2701@gmail.com>
2021-09-27 12:46:44 +0530

    minor fixes

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
+
 import styles from "../styles/not-found.module.css";
 
 export default function Custom404Page() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Page Not Found | Gallery Of JE</title>
+      </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>Page not found</h1>
         <span>
